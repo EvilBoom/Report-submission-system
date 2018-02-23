a02003df1cbd6296741ce43a15f24fdd8cfc1b5a
EvilBoom <1049142293@qq.com>
2018-02-23 11:02:11 +0800

    2b

--- hello.py
+++ hello.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return '<h1>hello word</h1>'
 
+@app.route('/usr/<name>')
+def user(name):
+    return '<h1>hello %s!</h1>'%name
+
 if __name__ == '__main__':
     app.run(debug=True)
