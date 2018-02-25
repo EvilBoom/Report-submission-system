4f3bababd7615f3615fc56d8b97b46c9ce4e05c0
EvilBoom <1049142293@qq.com>
2018-02-25 18:00:37 +0800

    4a

--- hello.py
+++ hello.py
@@ -1,16 +1,30 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import  Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY']= 'Torpedo'
 
 bootstrap = Bootstrap(app) 
-
 moment = Moment(app)
 
-@app.route('/')
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    name =  none
+    form = NameForm()
+    if form.validators_on_submit():
+        name = form.name.data
+        form.name.data=''
+        return render_template('index.html',form = form ,name = name)
 
 
 @app.route('/usr/<name>')
