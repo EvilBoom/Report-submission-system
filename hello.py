03f75c327be416d5d62e8d688b572010645819e9
EvilBoom <1049142293@qq.com>
2018-02-25 17:10:41 +0800

    3e

--- hello.py
+++ hello.py
@@ -1,21 +1,27 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import  Moment
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app) 
 
+moment = Moment(app)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
+
 
 @app.route('/usr/<name>')
 def user(name):
     return render_template('use.html',name=name)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
+
 
 @app.error_handlers(500)
 def internal_server_error(e):
