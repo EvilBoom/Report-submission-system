98d6158d8f8e6da4c5294925344c566401582238
EvilBoom <1049142293@qq.com>
2018-03-04 08:59:11 +0800

    4c

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,session,redirect,url_for
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_bootstrap import Bootstrap
 from flask_moment import  Moment
 from flask_wtf import FlaskForm
@@ -21,7 +21,10 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validators_on_submit():
-        session['name'] = form.naem.data
+        old_name = sesson.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html',form = form ,name = session.get('name'))
 
