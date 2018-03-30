a17721dc4fa2718aa36abe1bdbad91dc2ac2ecd5
EvilBoom <1049142293@qq.com>
2018-03-30 21:50:55 +0800

    run1

--- flasky.py
+++ flasky.py
@@ -2,13 +2,15 @@ import os
 from app import create_app, db
 from app.models import User, Role
 from flask_migrate import Migrate
+from flask_script import Manager
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+manager = Manager(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role)
 
 @app.cli.command()
 def test():
@@ -16,3 +18,5 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+if __name__ == '__main__':
+    manager.run()
--- manager.py
+++ manager.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-from flask_script import Manager, Shell
-from app import create_app, db
+from flask_script import Manager
+from app import create_app
 
 manager = Manager(app)
-manager.add_command("shell", Shell(make_context = make_shell_context))
-"""manager.add_command('db', MigrateCommand)"""
 
 def main():
     manager.run()
