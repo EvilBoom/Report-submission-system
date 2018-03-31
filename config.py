e2c761e459ee47a23f2c8fe4001514485a221e72
EvilBoom <1049142293@qq.com>
2018-03-31 16:35:45 +0800

    debug

--- config.py
+++ config.py
@@ -23,18 +23,18 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'postgresql://torpedo:zby6250196@localhost:5432/mydb'
+        'postgresql://tt:123456@localhost:5432/flask'
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'postgresql://torpedo:zby6250196@localhost:5432/mydb'
+        'postgresql://tt:123456@localhost:5432/flask'
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://torpedo:zby6250196@localhost:5432/mydb'
+        'postgresql://tt:123456@localhost:5432/flask'
 
 
 config = {
