20b0591f402699f368df421a7d79823fce84fd59
EvilBoom <1049142293@qq.com>
2018-04-01 16:59:18 +0800

    debug

--- app/main/views.py
+++ app/main/views.py
@@ -11,16 +11,14 @@ def index():
     if form.validate_on_submit():
         user=User.query.filter_by(username=form.name.data).first()
         if user is None :
-            user_role = Role(name = form.name.data)
-            user = User(username = form.name.data, role = user_role)
-            db.session.add_all([user_role, user])
-            db.session.commit()
+            user = User(username = form.name.data)
+            db.session.add(user)
             session['known'] = False
-            if current_app.config['FLASK_ADMIN']:
-                send_email(current_app.config['FLASK_ADMIN'],'New User',
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'],'New User',
                            'mail/new_user',user=user)
         else:
             session['known'] = True
         session['name']=form.name.data
-        return rediect(url_for('.index'))
+        return redirect(url_for('.index'))
     return render_template('index.html', form=form, name=session.get('name'), known=session.get('known',False))
