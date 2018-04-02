79135b42ecbfef9eff986cccade63f120b46b00f
Evilboom <1049142293@qq.com>
2018-04-02 16:53:14 +0800

    debug

--- kk/config.py
+++ kk/config.py
@@ -1,11 +1,11 @@
 class Config(object):
-    pass
+    SECRET_KEY = '736670cb10a600b695a55839ca3a5aa54a7d7356cdef815d2ad6e19a2031182b'
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://tt:123456@localhost:5432/flask"    
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://tt:123456@localhost:5432/flask"    
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
